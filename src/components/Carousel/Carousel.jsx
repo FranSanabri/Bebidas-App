58e55df35033f8add6c3807a659fb2b250f5a0de
FranSanabri <death.the.system@gmail.com>
2023-06-15 17:46:29 -0300

    Home-Casifinalizado

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './Carousel.css';
-import Carousel from "react-elastic-carousel";
+import Product from "./Product";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 function CarouselComponent() {
   const images = [
@@ -25,19 +27,23 @@ function CarouselComponent() {
     return () => clearTimeout(time);
   }, [img]);
 
-  const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2 },
-    { width: 768, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 },
-  ];
-
-  const Item = ({ children }) => {
-    return (
-      <div className="carousel-item">
-        <h3>{children}</h3>
-      </div>
-    );
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1200 },
+      items: 4,
+      partialVisibilityGutter: 40,
+      slidesToSlide: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1199, min: 768 },
+      items: 3,
+      partialVisibilityGutter: 30,
+    },
+    mobile: {
+      breakpoint: { max: 767, min: 0 },
+      items: 2,
+      partialVisibilityGutter: 20,
+    },
   };
 
   return (
@@ -78,26 +84,52 @@ function CarouselComponent() {
           )}
         </div>
       </div>
-      <div>
-      <h2 className="text-slider">Nuestras mejores ofertas, al mejor precio!</h2>
-        <Carousel breakPoints={breakPoints} className='carousel-2'>
-          <Item><img src="https://i.ibb.co/2SypNwY/1.png" alt="" className="w-full" /></Item>
-          <Item><img src="https://i.ibb.co/VmMZ00f/2.png" alt="" className="w-full" /></Item>
-          <Item><img src="https://i.ibb.co/HX9K9q9/3.png" alt="" className="w-full" /></Item>
-          <Item><img src="https://i.ibb.co/gRKnhFf/1.png" alt="" className="w-full" /></Item>
-          <Item><img src="https://i.ibb.co/4McB6rG/2.png" alt="" className="w-full" /></Item>
-          <Item><img src="https://i.ibb.co/Sw2k6CY/3.png" alt="" className="w-full" /></Item>
-          <Item><img src="https://i.ibb.co/2SypNwY/1.png" alt="" className="w-full" /></Item>
-          <Item><img src="https://i.ibb.co/VmMZ00f/2.png" alt="" className="w-full" />
-</Item>
+      <div className="carousel-content">
+        <h2 className="text-slider">Nuestras mejores ofertas, al mejor precio!</h2>
+        <Carousel responsive={responsive} className="carousel-2">
+          <Product
+            url="https://i.ibb.co/2SypNwY/1.png"
+            name="Nombre del producto"
+            price="Precio del producto"
+            description="Descripción del producto"
+          />
+          <Product
+            url="https://i.ibb.co/VmMZ00f/2.png"
+            name="Nombre del producto"
+            price="Precio del producto"
+            description="Descripción del producto"
+          />
+          <Product
+            url="https://i.ibb.co/HX9K9q9/3.png"
+            name="Nombre del producto"
+            price="Precio del producto"
+            description="Descripción del producto"
+          />
+          <Product
+            url="https://i.ibb.co/gRKnhFf/1.png"
+            name="Nombre del producto"
+            price="Precio del producto"
+            description="Descripción del producto"
+          />
+          <Product
+            url="https://i.ibb.co/4McB6rG/2.png"
+            name="Nombre del producto"
+            price="Precio del producto"
+            description="Descripción del producto"
+          />
+          <Product
+            url="https://i.ibb.co/Sw2k6CY/3.png"
+            name="Nombre del producto"
+            price="Precio del producto"
+            description="Descripción del producto"
+          />
+          <Product
+            url="https://i.ibb.co/2SypNwY/1.png"
+            name="Nombre del producto"
+            price="Precio del producto"
+            description="Descripción del producto"
+          />
         </Carousel>
-      </div>
-      <div className="carousel-slider"></div>
-      <div className="slider">
-
-        <div className="slide-track">
-          {/* Resto del código de los slides */}
-        </div>
       </div>
     </div>
   );
